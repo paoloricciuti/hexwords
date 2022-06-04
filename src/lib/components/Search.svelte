e6e6998f042475a25a32c87a12d91734a894cce8
Paolo Ricciuti <ricciutipaolo@gmail.com>
2022-06-04 18:42:36 +0200

    Fix search

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -2,10 +2,24 @@
     export let search: string;
 </script>
 
-<input type="search" bind:value={search} />
+<input type="search" bind:value={search} placeholder="search..." />
 
 <style>
     input {
+        width: min(30rem, 90vw);
         margin: 1rem auto;
+        display: block;
+        box-shadow: 0 0 0.3rem 0 rgba(0 0 0 / 0.3);
+        outline: 0;
+        border: 0;
+        border-radius: 10rem;
+        padding: 0.3rem;
+        transition: outline 75ms, outline-offset 75ms;
+        outline-offset: 0rem;
+        outline: 0;
+    }
+    input:focus {
+        outline: 1px solid black;
+        outline-offset: 0.25rem;
     }
 </style>
